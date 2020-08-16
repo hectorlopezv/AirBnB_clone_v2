00f7635385767afb698e8073e03fe7fcaa47752b
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-15 19:00:24 -0500

    Revert "making it run"
    This reverts commit 106759d4f9c08360ec89d7770f784dfb4309084c.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user and grant privileges on TABLES
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXIST 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.*  TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
