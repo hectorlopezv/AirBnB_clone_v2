60bf3ffd86633a1e41218f0c8caadad6b5707963
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 05:49:17 -0500

    adding review

--- models/review.py
+++ models/review.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
 
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id", ondelete="CASCADE"), nullable=False)
     user_id = Column(String(60),ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     
