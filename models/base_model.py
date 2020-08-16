1a9638b71be209abda20d6734c591f91cbd12de2
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-15 19:00:24 -0500

    Revert "adding"
    This reverts commit a5b9eecd0d0b39d81a303a1da3793e896630ba4e.

--- models/base_model.py
+++ models/base_model.py
@@ -2,40 +2,25 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-import inspect
+
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-
-        x_ = inspect.stack()[1].function #get the methods that call this function
-        #print(x_)
-        #print(type(x_))
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)#ADD TO FILE_OBJETCS
-        elif x_ == 'reload':
-            #print("ENTRE AQUI")
+            storage.new(self)
+        else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-
-        elif kwargs and x_ != 'reload':
-            #print("creo un nuevo usuario airbnv v2")
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            self.__dict__.update(kwargs)
-            storage.new(self)#ADD TO FILE_OBJETCS
-
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -46,7 +31,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()#to .json
+        storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
