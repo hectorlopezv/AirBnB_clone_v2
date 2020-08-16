dac0d97a0a7301ae3021846c3bd2b1525acf9a6f
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-15 19:00:24 -0500

    Revert "fixing"
    This reverts commit 7f3c4b9404ada7ddaf70b9e0f4c4a82f3bb87e75.

---
+++ jhonas/setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- the create databese
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- CREATE USER
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- THE ALL PRIVILEGES TO USER
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
