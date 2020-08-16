25f6a000fcc00ab36951db0f9c76ccfc82766db4
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 04:12:27 -0500

    adding users requirements

--- models/user.py
+++ models/user.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         
