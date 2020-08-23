a16397bd06f2aa82e6bebfd9ce2d36c96635af91
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-22 22:45:46 -0500

    adding setup for static  #0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ mkdir -p /data/web_static/shared/
 echo "Holberton Schol" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i   '/server_name _;/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sed -i   '/server_name _;/a location /hbnb_static { alias /data/web_static/current/; \n\tautoindex off; }' /etc/nginx/sites-available/default
 sudo nginx -s reload
 
 
