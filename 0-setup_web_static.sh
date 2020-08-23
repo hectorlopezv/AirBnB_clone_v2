7e5ceba798a868b09200b2cde26ae629af926200
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-22 23:11:11 -0500

    adding setup for static  #0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-#prepare your web servers
-apt install nginx -y
+# installs nginx
+apt-get -y update
+apt-get -y install nginx
+sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default
+sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
+sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default
+# echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
+service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton Schol" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "Hello Holberton School!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i   '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/; \n\tautoindex off; }' /etc/nginx/sites-available/default
-sudo nginx -s reload
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+service nginx restart
 exit 0
-
-
-
