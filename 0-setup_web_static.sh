67814dd40766abc1e7434a74ee289375bb90105c
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-22 22:47:16 -0500

    adding setup for static  #0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i   '/server_name _;/a location /hbnb_static { alias /data/web_static/current/; \n\tautoindex off; }' /etc/nginx/sites-available/default
 sudo nginx -s reload
+exit 0
 
 
 
