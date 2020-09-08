5b65d489544786374499db34ea18df72ebdcb60c
Your Name <you@example.com>
2020-09-08 23:43:02 +0000

    first commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
     """hello world"""
     return 'Hello HBNB!'
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
