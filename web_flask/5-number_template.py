6ae516b2388170d62ee9ea0fbc9727659339ed8d
Your Name <you@example.com>
2020-09-09 01:43:14 +0000

    #5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Flask framwework ligth weight"""
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """hello world"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_world_2():
+    """hello world"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_world_3(text):
+    """hello world"""
+    return 'C %s' % escape(text.replace('_', ' '))
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def hello_world_4(text='is cool'):
+    """hello world"""
+    return 'Python %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def hello_world_5(n):
+    """hello world"""
+    return '%s is a number' % n
+
+
+@app.route('/number_template/<int:n>')
+def hello_world_6(n):
+    """hello world"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
