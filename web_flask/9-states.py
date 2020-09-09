08bc9d8ee85d0264f0e13781921bc9be20680a2c
Ubuntu <ubuntu@gc-1664-web-01-1597877760.c.hbtn-students-instances.internal>
2020-09-09 03:59:48 +0000

    9

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Flask framwework ligth weight"""
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def pop_db(x):
+    """storage closing"""
+    storage.close()
+
+@app.route("/states")
+def hello_1():
+    """hello display all states like usual"""
+    state = storage.all(State)
+    return render_template('9-state.html', state=state)
+       
+
+@app.route("/states/<int:id>")
+def hello_1(id):
+    """return dict if id found else ..."""
+    for jsn in storage.all(State).values():
+         if jsn.id == id:
+            return render_template('9-state.html', state=jsn)
+    return render_template('9-state.html')
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    storage.reload()
+    app.run(host='0.0.0.0', port='5000')
