0b26d6b9a5af5fd3bfa5de3394fa3a1c4d4813b2
Your Name <you@example.com>
2020-09-09 02:49:59 +0000

    8 states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Flask framwework ligth weight"""
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def pop_db:
+    """storage closing"""
+    storage.close()
+
+
+@app.route('/number_template/states_list')
+def hello_world_6():
+    """hello world"""
+    state = list(storage.all(State).values())
+    return render_template('7-states_list.html', state=state)
+
+if __name__ == '__main__':
+    app.run(debug=True)
